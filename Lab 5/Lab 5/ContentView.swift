96ef63e5aaa716985c32f4627d16adcf0e2691d2
Cary Wang <56293984+wangcary19@users.noreply.github.com>
2022-10-12 12:15:39 -0400

    Update Lab 5

--- Lab 5/Lab 5/ContentView.swift
+++ Lab 5/Lab 5/ContentView.swift
@@ -1,88 +1,77 @@
 //
 //  ContentView.swift
-//  Lab 5
+//  weatherCities
 //
-//  Created by Cary Wang on 10/12/22.
+//  Created by Nick Kounavelis on 10/5/22.
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State private var cityCode: String = ""
+    
     var body: some View {
+                
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
+                    
+           VStack {
+                   
+               //--------------------------------------------------------------------
+               Button(action: {
+                       
+                        // no Swift code yet
+                       
+               } , label: {
+                   NavigationLink(destination: WeatherCityView(cityCode: "KBOS")) {
+                           Text("BOSTON")
+                               .padding()
+                               .foregroundColor(Color.blue)
+                               .font(.largeTitle)
+                   }
+               }) // end button
+               
+               //---------------------------------------------------------------------
+               Button(action: {
+                   
+                    // no Swift code yet
+                   
+               } , label: {
+                   NavigationLink(destination: WeatherCityView(cityCode: "KNYC")) {
+                       Text("NEW YORK CITY")
+                           .padding()
+                           .foregroundColor(Color.blue)
+                           .font(.largeTitle)
+                   }
+               }) // end button
+            
+               //---------------------------------------------------------------------
+               Button(action: {
+                   
+                    // no Swift code yet
+                   
+               } , label: {
+                   NavigationLink(destination: WeatherCityView(cityCode: "KMIA")) {
+                       Text("MIAMI")
+                           .padding()
+                           .foregroundColor(Color.blue)
+                           .font(.largeTitle)
+                   }
+               }) // end button
+                
+           } // end VStack
+           .navigationTitle("WEATHER - CITIES")
+                    
+        } // end navigation view
+        
     }
+    // end body
+     
+} // end struct
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-}
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
---
+++ Lab 5/Lab 5/WeatherCityView.swift
@@ -0,0 +1,39 @@
+// Cary Wang, for Professor Kounavelis's CISC 5140
+// October 12th, 2022
+
+import SwiftUI
+
+struct WeatherCityView: View {
+    
+    var cityCode: String
+    
+    let urlString = "http://w1.weather.gov/xml/current_obs/" + cityCode + ".xml"
+        
+    guard let url = URL(string: urlString)
+    else {
+        print("Hey Man, THIS URL DOES NOT WORK!")
+        exit(-1)
+    }
+        
+    // Perform HTTP Request
+    let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+        // Check for Error
+        if let error = error {
+            print("Error took place \(error)")
+            exit(-2)
+        }
+         
+        // Convert HTTP Response Data to a String
+        if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                
+            print("\(dataString)")
+                    
+        } // end let data
+    }
+                    
+    task.resume()
+    
+    var body: some View {
+        
+    }
+}
