d52ab261a09b1c62ef3e4be5dfbb58a1831f1fc4
Cary Wang <56293984+wangcary19@users.noreply.github.com>
2022-10-24 16:26:01 -0400

    Update Assets

--- Shelffle/Shelffle/ContentView.swift
+++ Shelffle/Shelffle/ContentView.swift
@@ -14,13 +14,15 @@ import CoreData
 //Global variables for viewing mode, font type, font size, and author randomization
 var isLight: Bool = false
 var font: String = ""
-var size: Int = 16
+var fsize: CGFloat = 16
 var randomize_authors: Bool = true
 
 //Custom colors for light and dark modes
 extension Color {
     static let BackgroundColor = Color("BackgroundColor")
-    static let TextColor = Color("TextColor") }
+    static let TextColor = Color("TextColor")
+    static let AuthorColor = Color("AuthorColor")
+}
 
 
 //--------------------------------------------------------------------------------------------
@@ -31,25 +33,76 @@ struct ContentView: View {
         //Background
         ZStack {
             Color.BackgroundColor.ignoresSafeArea()
-            
             //Text and settings
             VStack {
                 
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                
                 //Title
-                Text("Init").foregroundColor(Color.TextColor).bold()
+                HStack {
+                    Spacer()
+                    Text("Hope is the thing with feathers")
+                        .foregroundColor(Color.TextColor)
+                        .bold()
+                        .font(.title)
+                        .frame(width: 360, height: 75, alignment: .topLeading)
+                }
                 
                 //Author, with live biography link
-                
+                HStack {
+                    Spacer()
+                    Button {
+                        print("Edit button was tapped")
+                    } label: {
+                        Text("Emily Dickinson")
+                            .foregroundColor(Color.AuthorColor)
+                            .frame(alignment: .topLeading)
+                    }
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                }
                 
                 //Poem
+                ScrollView {
+                    HStack {
+                        Spacer()
+                        Text("\"Hope\" is the thing with feathers \n That perches in the soul - \n And sings the tune without the words \n And never stops at all - ")
+                            .foregroundColor(Color.TextColor)
+                            .bold()
+                            .font(.system(size: fsize))
+                            .frame(width: 360, height: 360, alignment: .topLeading)
+                            .multilineTextAlignment(.leading)
+                    }
+                }
                 
-                /*
                 //Settings
                 HStack {
-                    Button(<#T##title: StringProtocol##StringProtocol#>, action: <#T##() -> Void#>)
-                    Button(<#T##title: StringProtocol##StringProtocol#>, action: <#T##() -> Void#>)
+                    Spacer()
+                    Button {
+                        print("Edit button was tapped")
+                    } label: {
+                        Image(systemName: "arrow.clockwise.circle.fill")
+                    }
+                    Spacer()
+                    Button {
+                        print("Edit button was tapped")
+                    } label: {
+                        Image(systemName: "filemenu.and.selection")
+                    }
+                    Spacer()
+                    Spacer()
+                    Spacer()
                 } //end HStack for settings
-                 */
                 
             }//end VStack for text and settings
         }//end ZStack for background
